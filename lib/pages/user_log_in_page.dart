bcf7fe8520b1899c955e0f9dbcc627f843a882fb
wu-chinese <merkurieffr@gmail.com>
2022-06-30 16:30:32 -0700

    added validation to the login page (kind of)

--- lib/pages/user_log_in_page.dart
+++ lib/pages/user_log_in_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:form_field_validator/form_field_validator.dart';
 
 import '../models/user.dart';
 import '../models/users.dart';
@@ -10,69 +11,88 @@ class UserLoginPage extends StatefulWidget {
 }
 
 class _UserLoginPageState extends State<UserLoginPage> {
+  GlobalKey<FormState> formkey = GlobalKey<FormState>();
+
   String _email = "";
   String _password = "";
+  bool _logInFail = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('User Login'),
       ),
-      body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-        Icon(
-          Icons.person,
-          size: 300,
-          //color: Colors.white,
-        ),
-        Padding(
-          padding: const EdgeInsets.fromLTRB(200, 0, 200, 0),
-          child: TextFormField(
-            initialValue: "",
-            decoration: const InputDecoration(
-              labelText: 'Email',
-              // labelStyle: TextStyle(color: Colors.white),
+      body: Center(
+        child: Form(
+          autovalidateMode: AutovalidateMode.onUserInteraction,
+          key: formkey,
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            const Icon(
+              Icons.person,
+              size: 300,
+              //color: Colors.white,
             ),
-            //style: TextStyle(color: Colors.white),
-            onChanged: (value) {
-              _email = value;
-            },
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.fromLTRB(200, 0, 200, 0),
-          child: TextFormField(
-            initialValue: "",
-            obscureText: true,
-            decoration: const InputDecoration(
-              labelText: 'Password',
-              //labelStyle: TextStyle(color: Colors.white),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(200, 0, 200, 0),
+              child: TextFormField(
+                validator: MultiValidator([
+                  EmailValidator(errorText: "Not a valid email"),
+                  RequiredValidator(errorText: "Email is required"),
+                ]),
+                initialValue: "",
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(),
+                  errorText:
+                      _logInFail ? "Email or password is incorrect" : null,
+                  // labelStyle: TextStyle(color: Colors.white),
+                ),
+                //style: TextStyle(color: Colors.white),
+                onChanged: (value) {
+                  _email = value;
+                },
+              ),
             ),
-            //style: TextStyle(color: Colors.white),
-            onChanged: (value) {
-              _password = value;
-            },
-          ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(200, 20, 200, 0),
+              child: TextFormField(
+                validator: MinLengthValidator(1,
+                    errorText: "Password must be at least 1 characters"),
+                initialValue: "",
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(),
+                  errorText:
+                      _logInFail ? "Email or password is incorrect" : null,
+                  //labelStyle: TextStyle(color: Colors.white),
+                ),
+                //style: TextStyle(color: Colors.white),
+                onChanged: (value) {
+                  _password = value;
+                },
+              ),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  for (User user in Users().users) {
+                    if (user.email == _email && user.password == _password) {
+                      _logInFail = false;
+                      if (user.permission.index >= Permission.student.index) {
+                        Navigator.popAndPushNamed(context, '/home');
+                      }
+                    } else {
+                      setState(() {
+                        _logInFail = true;
+                      });
+                    }
+                  }
+                },
+                child: const Text('Login')),
+          ]),
         ),
-        TextButton(
-            onPressed: () {
-              for (User user in Users().users) {
-                if (user.email == _email && user.password == _password) {
-                  if (user.permission.index >= Permission.student.index) {
-                    Navigator.popAndPushNamed(context, '/wing_training');
-                  } /*else if (user.permission == Permission.training_shop) {
-                    Navigator.pushNamed(context, '/training_shop');
-                  } else if (user.permission == Permission.instructor) {
-                    Navigator.pushNamed(context, '/instructor');
-                  } else if (user.permission == Permission.student) {
-                    Navigator.pushNamed(context, '/student');
-                  }*/
-                  // Navigator.pushNamed(context, '/home');
-                }
-              }
-              // print("password: $_password");
-            },
-            child: const Text('Login')),
-      ]),
+      ),
     );
   }
 }
