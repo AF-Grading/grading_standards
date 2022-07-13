fbc4293f777f2667e84ab933f797769f6333f871
wu-chinese <merkurieffr@gmail.com>
2022-07-13 10:16:03 -0700

    grade_sheet_page potential changes

--- lib/pages/grade_sheet_page.dart
+++ lib/pages/grade_sheet_page.dart
@@ -233,3 +233,185 @@ class GradeSheetPage extends StatelessWidget {
     );
   }
 }
+
+
+// import 'package:app_prototype/pages/edit_gradesheet_page.dart';
+
+// import '../models/grade_enums.dart';
+// import '/models/grade_sheet.dart';
+// import 'package:flutter/material.dart';
+
+// import 'add_edit_grade_sheet_page.dart';
+
+// class GradeSheetPage extends StatelessWidget {
+//   const GradeSheetPage({Key? key, required this.gradeSheet}) : super(key: key);
+
+//   final GradeSheet gradeSheet;
+//   static double dist = 35;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Row(
+//           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//           children: [
+//             Text("Instructor: ${gradeSheet.instructor.name}"),
+//             GestureDetector(
+//               child: const Icon(Icons.edit),
+//               onTap: () => Navigator.push(
+//                 context,
+//                 MaterialPageRoute(
+//                   builder: (context) => AddEditGradeSheetPage(
+//                     gradeSheet: gradeSheet,
+//                   ),
+//                 ),
+//               ),
+//             )
+//           ],
+//         ),
+//       ),
+//       body: SingleChildScrollView(
+//         child: Column(
+//           children: [
+//             Card(
+//               child: ExpansionTile(
+//                 title: const Text("Overall"),
+//                 initiallyExpanded: true,
+//                 children: [
+//                   Padding(
+//                     padding: const EdgeInsets.all(8.0),
+//                     child: Row(
+//                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                       children: [
+//                         Wrap(children: [
+//                           const Text("Student: "),
+//                           Text(gradeSheet.student.name),
+//                         ]),
+//                         Wrap(children: [
+//                           const Text("Mission Number: "),
+//                           Text(gradeSheet.missionNum.toString()),
+//                         ])
+//                       ],
+//                     ),
+//                   ),
+//                   Padding(
+//                     padding: const EdgeInsets.all(8.0),
+//                     child: Row(
+//                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                       children: [
+//                         Wrap(children: [
+//                           const Text("Sortie Number"),
+//                           Text(gradeSheet.sortieNumber.toString()),
+//                         ]),
+//                         Wrap(children: [
+//                           const Text("Flight Duration"),
+//                           Text(gradeSheet.length)
+//                         ]),
+//                       ],
+//                     ),
+//                   ),
+//                   Padding(
+//                     padding: const EdgeInsets.all(8.0),
+//                     child: Row(
+//                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                       children: [
+//                         Wrap(children: [
+//                           const Text("Weather: "),
+//                           Text(gradeSheet.weather.name)
+//                         ]),
+//                         Wrap(children: [
+//                           const Text("Sortie Profile: "),
+//                           Text(gradeSheet.profile)
+//                         ]),
+//                       ],
+//                     ),
+//                   ),
+//                   Padding(
+//                     padding: const EdgeInsets.all(8.0),
+//                     child: Row(
+//                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                       children: [
+//                         Wrap(children: [
+//                           const Text("Overall Grade: "),
+//                           Text("${gradeSheet.overall.index - 2}"),
+//                         ]),
+//                         Wrap(children: [
+//                           const Text("Comments: "),
+//                           Text(gradeSheet.overallComments),
+//                         ])
+//                       ],
+//                     ),
+//                   ),
+//                   Padding(
+//                       padding: const EdgeInsets.all(8.0),
+//                       child: Wrap(children: [
+//                         const Text("Recommendations"),
+//                         Text(gradeSheet.recommendations),
+//                       ])),
+//                   Padding(
+//                     padding: const EdgeInsets.all(8.0),
+//                     child: Row(
+//                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                         children: [
+//                           Wrap(
+//                             children: [
+//                               const Text("Day/Night: "),
+//                               Text(gradeSheet.dayNight.name),
+//                             ],
+//                           ),
+//                           Wrap(children: [
+//                             const Text("Date: "),
+//                             Text(
+//                                 "${gradeSheet.startTime.year}/${gradeSheet.startTime.month}/${gradeSheet.startTime.day}"),
+//                           ]),
+//                         ]),
+//                   ),
+//                   Padding(
+//                     padding: const EdgeInsets.all(8.0),
+//                     child: Row(
+//                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                         children: [
+//                           // TODO: change expanded to flexible for listtiles casue they fat
+
+//                           Wrap(children: [
+//                             Text("Pilot Qualifications: "),
+//                             Text(gradeSheet.pilotQual == PilotQual.noSelection
+//                                 ? ""
+//                                 : gradeSheet.pilotQual.name),
+//                           ]),
+//                           Wrap(children: [
+//                             Text("AD Qualifications: "),
+//                             Text(gradeSheet.adQual == AdQual.noSelection
+//                                 ? ""
+//                                 : gradeSheet.adQual.name),
+//                           ]),
+//                           Wrap(children: [
+//                             Text("Sortie Type: "),
+//                             Text(gradeSheet.sortieType.name),
+//                           ]),
+//                         ]),
+//                   ),
+//                 ],
+//               ),
+//             ),
+//             Card(
+//               child: ExpansionTile(
+//                 title: const Text("Graded Items"),
+//                 initiallyExpanded: true,
+//                 children: gradeSheet.grades
+//                     .where((item) => item.grade != Grade.noGrade)
+//                     .map((item) => ListTile(
+//                           leading: Text("${item.grade.index - 2}"),
+//                           title: Text(item.name),
+//                           subtitle: Text(item.comments),
+//                         ))
+//                     .toList(),
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
